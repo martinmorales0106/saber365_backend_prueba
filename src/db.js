b3cb6eb11c9bc9e374a64eac39caf209c163c547
“martinmorales0106” <marmorji123456@gmail.com>
2024-06-17 19:11:46 -0500

    db conect2

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL, DB_PORT } = process.env;
 
 // crear una instancia de conexión a una base de datos PostgreSQL
 // const sequelize = new Sequelize(
